0613c26f2407c123028747f61a97df19d69b24c0
Logan Jeon <jeonyg@gmail.com>
2018-08-08 20:56:57 +0900

    Modify script for cscope.

--- mkcscope_bb_recipe.sh
+++ mkcscope_bb_recipe.sh
@@ -3,4 +3,4 @@ rm -rf cscope.out cscope.files
 
 find . \( -name '*.bb' -o -name '*.bbappend' -o -name '*.bbclass' -o -name '*.conf' -o -name '*.inc' -o -name '*.service' -o -name '*.py' -o -name '*.sh' -o -name '*.ini' -o -name '*.patch' \) -print > cscope.files
 
-cscope -I cscope.files
+cscope -i cscope.files
--- mkcscope_inc_patch.sh
+++ mkcscope_inc_patch.sh
@@ -3,4 +3,4 @@ rm -rf cscope.out cscope.files
 
 find . \( -name '*.[chsS]' -o -name '*.c[px][px]' -o -name '*.h[px][px]' -o -name '*.cc' -o -name '*.hh' -o -name '*.asm' -o -name '*.[Mm]ake' -o -name '*.fidl' -o -name '*.fdepl' -o -name '*.bb' -o -name '*.bbappend' -o -name '*.xml' -o -name '*.cfg' -o -name '*.mk' -o -name '[Mm]akefile' -o -name '*.ini' -o -name '*.inc' -o -name '*.cmake' -o -name 'CMakeLists.txt' -o -name '*.a[mc]' -o -name '*.in' -o -name '*.conf' -o -name '*.sh' -o -name '*.patch' \) -print > cscope.files
 
-cscope -I cscope.files
+cscope -i cscope.files
